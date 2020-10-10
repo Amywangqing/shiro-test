6447959a6e556951222a6a978ca1c24f37f7daf8
Administrator <Administrator@PC>
2020-10-10 16:04:06 +0800

    修改UserController

--- src/main/java/com/qingfeng/controller/UserController.java
+++ src/main/java/com/qingfeng/controller/UserController.java
@@ -1,5 +1,14 @@
 package com.qingfeng.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
 
+@RequestMapping("/hello")
+public String hello() {
+	return "Hello";
+}
+	
 }
